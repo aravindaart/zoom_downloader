08742ed3e9484396cb2afc42e3dd5d120ad52cb3
a0m0fxo <aravindhan.mohan@walmartlabs.com>
2021-04-14 13:00:54 +0530

    version 1.1

--- popup.js
+++ popup.js
@@ -2,17 +2,24 @@ document.getElementById("zoomDownloader").addEventListener('click', () => {
     console.log("Popup DOM fully loaded and parsed");
 
     function modifyDOM() {
-        var aTag = document.createElement("a");
-        aTag.innerHTML = "Click here to download the video";
-        aTag.setAttribute('download', "download");
-        aTag.setAttribute("target", "_blank");
-        aTag.href = document.getElementsByTagName('video')[0].src;
-        aTag.style.color = "white";
-        aTag.style.background = "#2D8CFF";
-        aTag.style.padding = "1rem";
-        var appDiv = document.getElementById('app');
-        appDiv.appendChild(aTag);
-        aTag.click();
+        var a = document.getElementById("download_button");
+        if (!a) {
+            var aTag = document.createElement("a");
+            aTag.innerHTML = "Click here to download the video";
+            aTag.setAttribute("target", "_blank");
+            aTag.setAttribute("download", "download");
+            aTag.setAttribute("id", "download_button");
+            aTag.href = document.getElementsByTagName('video')[0].src;
+            aTag.style.color = "white";
+            aTag.style.background = "#2D8CFF";
+            aTag.style.padding = "1rem";
+            // var appDiv = document.getElementById('app');
+            var appDiv = document.getElementsByTagName('div')[0];
+            appDiv.appendChild(aTag);
+            aTag.click();
+        }else{
+            a.click()
+        }
         return "Use CLICK ME to download";
     }
 
